027584db33d41a1c74c28fa211b3ae563cf1a02b
Italo Colt <103470585+ilima12@users.noreply.github.com>
2022-11-22 12:15:12 -0300

    ajustando a imagem topo

--- src/telas/cesta.js
+++ src/telas/cesta.js
@@ -1,6 +1,19 @@
+import React from 'react'
+import {StyleSheet, Image, Dimensions} from 'react-native'
 
-import {Text} from 'react-native'
+import topo from '../../assets/topo.png';
+
+const width = Dimensions.get('screen').width;
 
 export default function Cesta() {
-    return <Text>Cesta</Text>
+    return <Image source={topo} style={estilos.topo}/>
 }
+
+
+const estilos = StyleSheet.create({
+    topo: {
+        width: "100%",
+        height: 578/768 * width,
+    }
+
+})
