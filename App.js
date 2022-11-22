6d3c040c65ac7224ddae95b034f33c2bccf77688
Italo Colt <103470585+ilima12@users.noreply.github.com>
2022-11-22 00:21:34 -0300

    Cesta

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import React from 'react';
+import Cesta from './src/telas/cesta';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Alura!</Text>
+      <Cesta />
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/telas/cesta.js
@@ -0,0 +1,6 @@
+
+import {Text} from 'react-native'
+
+export default function Cesta() {
+    return <Text>Cesta</Text>
+}
