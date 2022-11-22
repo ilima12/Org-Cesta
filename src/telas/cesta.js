81e07e3560c7e37a55a240155f197824399a3760
Italo Colt <103470585+ilima12@users.noreply.github.com>
2022-11-22 14:16:26 -0300

    implementando css no titulo

--- src/telas/cesta.js
+++ src/telas/cesta.js
@@ -1,12 +1,15 @@
 import React from 'react'
-import {StyleSheet, Image, Dimensions} from 'react-native'
+import {StyleSheet, Image, Dimensions, Text} from 'react-native'
 
 import topo from '../../assets/topo.png';
 
 const width = Dimensions.get('screen').width;
 
 export default function Cesta() {
-    return <Image source={topo} style={estilos.topo}/>
+    return <>
+    <Image source={topo} style={estilos.topo}/>
+    <Text style={estilos.titulo}>Detalhes de cesta</Text>
+    </>
 }
 
 
@@ -14,6 +17,16 @@ const estilos = StyleSheet.create({
     topo: {
         width: "100%",
         height: 578/768 * width,
+    },
+    titulo: {
+        width: "100%" ,
+        position: "absolute",
+        textAlign: "center",
+        fontSize: 16,
+        lineHeight: 26,
+        color: "white",
+        fontWeight: "bold",
+        padding: 16
     }
 
 })
