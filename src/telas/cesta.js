52c8648b58b126a54e143d1eb58dcadddec2b5f7
Italo Colt <103470585+ilima12@users.noreply.github.com>
2022-11-24 12:40:16 -0300

    Detalhes da cesta
    css da descrição, nomeFazenda e seleç~~ao da imagem da fazenda no nome

--- src/telas/cesta.js
+++ src/telas/cesta.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {StyleSheet, Image, Dimensions, Text, View} from 'react-native'
 
 import topo from '../../assets/topo.png';
+import logo from '../../assets/logo.png';
 
 const width = Dimensions.get('screen').width;
 
@@ -10,15 +11,16 @@ export default function Cesta() {
     <Image source={topo} style={estilos.topo}/>
     <Text style={estilos.titulo}>Detalhes de cesta</Text>
     
-    <View>
+    <View style={estilos.cesta}>
+        <Image source={logo} />
         <Text style={estilos.nome}>Cesta de Verduras</Text>
-        <Text>Jenny Jack Farm</Text>
-        <Text>
+        <Text style={estilos.nomeFazenda}>Jenny Jack Farm</Text>
+        <Text style={estilos.descricao}>
             Uma cesta com produtos selecionados
             cuidadosamente da fazenda direto para
             sua cozinha
         </Text>
-        <Text>R$ 40,00</Text>
+        <Text style={estilos.preco}>R$ 40,00</Text>
     </View>
     </>
 }
@@ -39,11 +41,32 @@ const estilos = StyleSheet.create({
         fontWeight: "bold",
         padding: 16
     },
+    cesta: {
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+    },
     nome: {
         color: "#464646",
         fontSize: 26,
         lineHeight: 42,
         fontWeight: "bold",
+    },
+    nomeFazenda: {
+        fontSize: 16,
+        lineHeight: 26,
+    },
+    descricao: {
+        color: "#A3A3A3",
+        fontSize: 16,
+        lineHeight: 26,
+        
+    },
+    preco: {
+        color: "#2A9F85",
+        fontWeight: "bold",
+        fontSize: 26,
+        lineHeight:42,
+        marginTop: 8,
     }
 
-})
+});
