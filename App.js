90a3cbc4e886a804406cbf55a5c636b62e74db9e
Italo Colt <103470585+ilima12@users.noreply.github.com>
2022-11-25 09:44:32 -0300

    alteração do C no caminho

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 
 import { StatusBar, SafeAreaView} from 'react-native';
 import React from 'react';
-import Cesta from './src/telas/Cesta';
+import Cesta from './src/telas/cesta';
 export default function App() {
   return (
     <SafeAreaView >
